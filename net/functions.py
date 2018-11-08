6810cce24ea9742e0e53ce6b42e0b2cf8dadf2bb
Victor Czech <victor.czech@t-online.de>
2018-11-08 21:57:49 +0100

    Layer, functions updated

---
+++ net/functions.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+# functions by Victor Czech, 2018
+
+"""
+Wrapper for activation and cost functions.
+Functions can be accessed via compute(x) and gradient(y), where number of 
+parameters may vary. List of available functions in specific class 
+constructor, see "function_set".
+Some functions, e.g. "sigmoid", take their output for faster gradient
+computing.
+"""
+
+
+import numpy as np
+
+
+class Function:
+
+    def __init__(self, name, function_set):
+
+        self.name = name
+        self.function = function_set[name]
+
+
+
+class ActivationFunction(Function):
+
+
+    @staticmethod
+    def sigmoid(x, gradient):
+
+        if not gradient:
+            return 1. / (1. + np.exp(-x))
+        else:
+            # x has to be sigmoid(x) !
+            return x*(1 - x)
+
+
+    def __init__(self, name):
+
+        # list of available functions
+        function_set = {
+            "sigmoid" : ActivationFunction.sigmoid
+        }
+
+        Function.__init__(self, name, function_set)
+
+
+    def gradient(self, y):
+
+        return self.function(y, gradient=True)
+
+    def compute(self, x):
+
+        return self.function(x, gradient=False)
+
+
+
+class CostFunction:
+
+    def __init__(self, name):
+        pass
+
+
+
+def main():
+
+    a = ActivationFunction("sigmoid")
+
+    x = 5
+
+    y = a.compute(x)
+    g = a.gradient(y) # gradient of x
+
+    print(x, y, g)
+
+
+
+
+if __name__ == "__main__":
+    main()
--- net/layer.py
+++ net/layer.py
@@ -3,3 +3,58 @@
 
 
 import numpy as np
+
+from functions import ActivationFunction
+
+
+# Layer object with basic forward / backward functionality
+
+class Layer:
+
+
+    def __init__(self, activation):
+
+        self.activation = ActivationFunction(activation)
+
+
+    @staticmethod
+    def init_with_weights(input_size, output_size, activation,
+        w_range, b_range):
+
+        layer = Layer(activation)
+
+        w_multiplier = np.abs(w_range[0] - w_range[1])
+        b_multiplier = np.abs(b_range[0] - b_range[1])
+
+        weights = w_multiplier * np.random.rand(output_size, input_size) \
+                + w_range[0]
+
+        biases = b_multiplier * np.random.rand(output_size, 1) \
+               + b_range[0]
+
+        layer.weights = np.c_[weights, biases]
+
+        return layer
+
+        
+
+    @staticmethod
+    def load():
+        pass
+
+
+
+def main():
+
+    l = Layer.init_with_weights(
+            input_size = 3,
+            output_size = 2,
+            activation = "sigmoid",
+            w_range = [-1,1],
+            b_range = [0.5,0.9]
+        )
+
+
+
+if __name__ == '__main__':
+    main()
